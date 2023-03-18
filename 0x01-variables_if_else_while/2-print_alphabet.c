e80cc03882e6e6a6ebe4b296f54bbabf88e8d7be
Zamantungwa <ntombzamakhumalo@gmail.com>
2023-03-17 19:18:03 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main betty documentation function
+ *
+ * Return: 0 if need be
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+		return (0);
+}
