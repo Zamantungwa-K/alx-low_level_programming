375fb4a4c8b2a3198f53b027250ffd6d5901eeb9
Zamantungwa <ntombzamakhumalo@gmail.com>
2023-03-17 19:50:53 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main betty documentation function
+ *
+ * Return: 0 if need be
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
