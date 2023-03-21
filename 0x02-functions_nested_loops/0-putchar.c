61206a75e413f9b9d825dfdbb9b40c3fb39f22da
Zamantungwa <ntombzamakhumalo@gmail.com>
2023-03-20 17:11:15 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - this is the main betty dcoumentation function
+ * _putchar: print the alphabets putchar
+ * Return: 0 if need be
+ */
+
+
+int main(void)
+{
+	_putchar('_');
+	 _putchar('p');
+	_putchar('u');
+	 _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+	 _putchar('\n');
+	 return (0);
+}
